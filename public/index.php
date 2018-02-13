c23aef5e252947ba58584670344ca052776f97f0
Vaalyn <vaalyndev@gmail.com>
2018-02-13 16:00:29 +0100

    Use object oriented syntax for container filling

--- public/index.php
+++ public/index.php
@@ -9,13 +9,13 @@
 
 	$app = new \Slim\App(require_once __DIR__ . '/../config/config.php');
 
-	$container                           = $app->getContainer();
-	$container['auth']                   = new \Service\Auth\Auth($container);
-	$container['database']               = EloquentFactory::create($container->config['database']);
-	$container['errorHandler']           = new ErrorHandler();
-	$container['flash']                  = new \Slim\Flash\Messages();
-	$container['phpErrorHandler']        = new ErrorHandler();
-	$container['renderer']               = new PhpRenderer('../template');
+	$container                         = $app->getContainer();
+	$container->auth                   = new \Service\Auth\Auth($container);
+	$container->database               = EloquentFactory::create($container->config['database']);
+	$container->errorHandler           = new ErrorHandler();
+	$container->flash                  = new \Slim\Flash\Messages();
+	$container->phpErrorHandler        = new ErrorHandler();
+	$container->renderer               = new PhpRenderer('../template');
 
 	$app->add(new RKA\Middleware\IpAddress(false, []));
 	$app->add(new Middleware\Auth\AuthMiddleware($container));
