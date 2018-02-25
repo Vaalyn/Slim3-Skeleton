d37eef02b6455cec54bbf311a11e5e5b4614ed43
Vaalyn <vaalyndev@gmail.com>
2018-02-25 18:39:23 +0100

    Added API login route

---
+++ routes/Api/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+	namespace Routes\Api;
+
+	use Psr\Container\ContainerInterface;
+	use Slim\Http\Request;
+	use Slim\Http\Response;
+
+	class LoginController {
+		/**
+		 * @var \Psr\Container\ContainerInterface
+		 */
+		protected $container;
+
+		/**
+		 * @param \Psr\Container\ContainerInterface $container
+		 */
+		public function __construct(ContainerInterface $container) {
+			$this->container = $container;
+		}
+
+		/**
+		 * @param \Slim\Http\Request $request
+		 * @param \Slim\Http\Response $response
+		 * @param array $args
+		 *
+		 * @return \Slim\Http\Response
+		 */
+		public function loginAction(Request $request, Response $response, array $args): Response {
+			$username = $request->getParsedBody()['username'] ?? null;
+			$password = $request->getParsedBody()['password'] ?? null;
+			$rememberMe = true;
+
+			if (!$this->container->auth->attempt($username, $password, $rememberMe)) {
+				return $response->write(json_encode(array(
+					'status' => 'error',
+					'errors' => $exception->getMessage()
+				)));
+			}
+
+			return $response->write(json_encode(array(
+				'status' => 'success'
+			)));
+		}
+	}
+?>
