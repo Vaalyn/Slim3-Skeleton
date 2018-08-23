2ca4758865e04477611de2cd2a91163a3d3806e1
Vaalyn <vaalyndev@gmail.com>
2018-08-23 20:36:13 +0200

    Order config entries alphabetically

--- config/config.example.php
+++ config/config.example.php
@@ -1,51 +1,51 @@
 <?php
 
 return [
-	'settings' => [
-		'determineRouteBeforeAppMiddleware' => true,
-		'displayErrorDetails' => false
-	],
 	'config' => [
-		'session' => [
-			'name'     => 'SLIM3_SKELETON_SESSID',
-			'lifetime' => 1200,
-			'path'     => '/',
-			'domain'   => 'localhost',
-			'secure'   => false,
-			'httponly' => true
-		],
-		'database' => [
-			'driver'    => '',
-			'host' 	    => '',
-			'database' 	=> '',
-			'username' 	=> '',
-			'password' 	=> '',
-			'charset'   => 'utf8mb4',
-			'collation' => 'utf8mb4_unicode_ci',
-			'prefix'    => '',
-			'port'      => 3306
-		],
 		'authentication' => [
 			'cookie' => [
-				'name'     => 'remember',
-				'expire'   => 2592000,
 				'domain'   => '',
-				'secure'   => false,
-				'httponly' => true
+				'expire'   => 2592000,
+				'httponly' => true,
+				'name'     => 'remember',
+				'secure'   => false
 			],
 			'host' => [
 				'ip' => '127.0.0.1'
 			],
-			'routes' => require_once __DIR__ . '/routes/authenticated.php',
-			'local' => require_once __DIR__ . '/routes/local.php'
+			'local' => require_once __DIR__ . '/routes/local.php',
+			'routes' => require_once __DIR__ . '/routes/authenticated.php'
 		],
 		'authorization' => [
-			'routes' => require_once __DIR__ . '/routes/authorized.php',
-			'authorizers' => require_once __DIR__ . '/authorizers.php'
+			'authorizers' => require_once __DIR__ . '/authorizers.php',
+			'routes' => require_once __DIR__ . '/routes/authorized.php'
+		],
+		'database' => [
+			'charset'   => 'utf8mb4',
+			'collation' => 'utf8mb4_unicode_ci',
+			'database' 	=> '',
+			'driver'    => '',
+			'host' 	    => '',
+			'password' 	=> '',
+			'port'      => 3306,
+			'prefix'    => '',
+			'username' 	=> ''
 		],
 		'menu' => require_once __DIR__ . '/menu.php',
+		'session' => [
+			'domain'   => 'localhost',
+			'httponly' => true,
+			'lifetime' => 1200,
+			'name'     => 'SLIM3_SKELETON_SESSID',
+			'path'     => '/',
+			'secure'   => false
+		],
 		'template' => [
 			'path' => __DIR__ . '/../template'
 		]
+	],
+	'settings' => [
+		'determineRouteBeforeAppMiddleware' => true,
+		'displayErrorDetails' => false
 	]
 ];
